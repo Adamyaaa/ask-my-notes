178d4a15f11c66b9bb1e19962a23bbef9eeee784
adamyaaa <arcticlunitic@gmail.com>
2026-08-23 17:19:26 +0530

    phase 1 done, now embedding's next

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 from pypdf import PdfReader
 
-reader = PdfReader("data/notes.pdf")
+def extract_pages(pdf_path):
+    reader = PdfReader(pdf_path)
 
-pages=[]
+    pages = []
 
-for index,page in enumerate(reader.pages):
-    page_data={
-        "page": index+1,
-        "text": page.extract_text()
-    }
-    pages.append(page_data)
+    for index, page in enumerate(reader.pages):
+        page_data = {
+            "page": index + 1,
+            "text": page.extract_text()
+        }
+        pages.append(page_data)
 
-chunks=[]
+    return pages
+pages = extract_pages("data/notes.pdf")
+
+
+
+def chunk_pages(pages,chunk_size,overlap):
+    chunks=[]
+    for page_data in pages:
+        text=page_data["text"].strip()
+        if text:
+            words=text.split()
+            for index,start in enumerate(range(0,len(words),chunk_size-overlap)):
+                chunk_words=words[start:start+chunk_size]
+
+                chunk_data={
+                    "chunk_id": index,
+                    "page": page_data["page"],
+                    "text": " ".join(chunk_words)
+                }
+                chunks.append(chunk_data)
+    return chunks
+
 chunk_size=100
 overlap=20
-
-for page_data in pages:
-    text=page_data["text"].strip()
-    if text:
-        words=text.split()
-        for index,start in enumerate(range(0,len(words),chunk_size-overlap)):
-            chunk_words=words[start:start+chunk_size]
-
-            chunk_data={
-                "chunk_id": index,
-                "page": page_data["page"],
-                "text": " ".join(chunk_words)
-            }
-            chunks.append(chunk_data)
-
+chunks=chunk_pages(pages,chunk_size,overlap)
 # print("number of pages:", len(pages))
 # print("number of chunks:", len(chunks))
 print(chunks[0])
